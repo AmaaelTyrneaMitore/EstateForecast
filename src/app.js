b38905a98d220c6257e42f9fa3bfa71ad099f95d
Shashank Singh <96770927+AmaaelTyrneaMitore@users.noreply.github.com>
2023-11-18 08:15:50 +0530

    🌟 Update: Implement Baisc KNN using TFJS
    - Add @tensorflow/tfjs-node as project dependency
    - Implement basic KNN regression to predict house price with TFJS

--- src/app.js
+++ src/app.js
@@ -1,23 +1,56 @@
 import { join } from 'path';
 import appRootPath from 'app-root-path';
+import { tensor } from '@tensorflow/tfjs-node';
 
 import loadCSV from './utils/csv-loader.js';
 
-const filename = join(appRootPath.path, 'data', 'kc_house_data.csv');
+// Load the CSV file path
+const csvFilePath = join(appRootPath.path, 'data', 'kc_house_data.csv');
 
-const options = {
-  dataColumns: ['lat', 'long'],
-  labelColumns: ['price'],
-  shuffle: true,
-  splitTest: 5,
+// Define options for loading CSV data
+const loadOptions = {
+  dataColumns: ['lat', 'long'], // Columns representing features
+  labelColumns: ['price'], // Column representing the label
+  shuffle: true, // Shuffle the data
+  splitTest: 10, // Number of rows for testing
 };
 
-const { features, labels, testFeatures, testLabels } = loadCSV(filename, options);
+// Load the CSV data using provided options
+let { features, labels, testFeatures, testLabels } = loadCSV(csvFilePath, loadOptions);
 
-console.log('Features:', features);
-console.log('Labels:', labels);
+// Convert loaded data into tensors
+const tensorFeatures = tensor(features);
+const tensorLabels = tensor(labels);
 
-if (testFeatures && testLabels) {
-  console.log('Test Features:', testFeatures);
-  console.log('Test Labels:', testLabels);
-}
+/**
+ * K-Nearest Neighbors algorithm for prediction.
+ *
+ * @param {tf.Tensor} features - Training features.
+ * @param {tf.Tensor} labels - Training labels.
+ * @param {tf.Tensor} predictionPoint - Point for prediction.
+ * @param {number} k - Number of neighbors to consider.
+ * @returns {number} - Predicted value.
+ */
+const knn = (features, labels, predictionPoint, k) => {
+  // Calculate distances between the prediction point and features
+  const distances = features.sub(predictionPoint).pow(2).sum(1).pow(0.5);
+
+  // Combine distances with labels, sort by distance, and take the top 'k' neighbors
+  const topK = distances
+    .expandDims(1)
+    .concat(labels, 1)
+    .unstack()
+    .sort((a, b) => (a.arraySync()[0] > b.arraySync()[0] ? 1 : -1))
+    .slice(0, k);
+
+  // Calculate the predicted value by averaging the 'k' nearest neighbors' labels
+  const prediction = topK.reduce((acc, pair) => acc + pair.arraySync()[1], 0) / k;
+
+  return prediction;
+};
+
+// Make a prediction using KNN for the first test feature with k=10
+const prediction = knn(tensorFeatures, tensorLabels, tensor(testFeatures[0]), 10);
+
+// Display the prediction and actual price
+console.log(`Prediction: $${prediction}\tActual Price: $${testLabels[0][0]}`);
